e88d347d4f8b33ca89be4c8d581362d898f6ffdf
Addison <addisonbennett@gmail.com>
2016-04-18 14:25:24 -0600

    creating particle field

---
+++ app.js
@@ -0,0 +1,30 @@
+var Particle = function (position){
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-0.5, 0.5), random(-0.5, 0.5));
+  this.position = position.copy();
+};
+
+Particle.prototype.run = function (){
+  this.update();
+  this.display();
+  this.intersects();
+};
+
+Particle.prototype.update = function (){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.acceleration.multi(-0.1);
+  if (this.position.x < 0) this.position.x =
+  width;
+  if (this.position.x > width) this.position.x
+  = 0;
+  if (this.position.y < 0) this.position.y =
+  height;
+  if (this.position.y > height) this.position.y = 0;
+};
+
+Particle.prototype.intersects = function ()
+{
+  for (var i=0; i < particles.length; i++)
+}
+var other = particles[i];
